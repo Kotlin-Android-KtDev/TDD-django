0f7ff6d004cbc5406445acd0e25a19ebf2b7a16e
Nut <s6101012630071@email.kmutnb.ac.th>
2020-01-01 15:39:13 +0700

    Chapter7:Ensuring We Have a Regression Test

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from lists.models import Item
+from django.shortcuts import redirect, render
+
 
 def home_page(request):
     if request.method == 'POST':
